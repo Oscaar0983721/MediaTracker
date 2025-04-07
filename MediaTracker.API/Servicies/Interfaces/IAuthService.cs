1bb70c03a97bb65388472e9c846269287589ed51
Andrés Araya Saborío <206283716+Roxana0976@users.noreply.github.com>
2025-04-06 18:44:27 -0600

    Create IAuthService.cs

---
+++ MediaTracker.API/Servicies/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using MediaTracker.API.Models;
+
+namespace MediaTracker.API.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<User> AuthenticateAsync(string username, string password);
+    }
+}
