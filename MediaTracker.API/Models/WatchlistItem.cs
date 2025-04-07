f6a903015a1772673be35cc887bd1ce92d9b8e62
Andrés Araya Saborío <206283716+Roxana0976@users.noreply.github.com>
2025-04-06 18:42:24 -0600

    Create WatchlistItem.cs

---
+++ MediaTracker.API/Models/WatchlistItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MediaTracker.API.Models
+{
+    public class WatchlistItem
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int MediaId { get; set; }
+        public string MediaType { get; set; }
+        public DateTime AddedAt { get; set; }
+    }
+}
