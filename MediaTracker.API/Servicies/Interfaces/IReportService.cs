1af2f6fa2e6b005a2087d3f49c97f8e7405d6bd2
Andrés Araya Saborío <206283716+Roxana0976@users.noreply.github.com>
2025-04-06 18:44:31 -0600

    Create IReportService.cs

---
+++ MediaTracker.API/Servicies/Interfaces/IReportService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MediaTracker.API.Services.Interfaces
+{
+    public interface IReportService
+    {
+        Task<object> GenerateReportAsync(string reportType, DateTime dateFrom, DateTime dateTo);
+        Task<byte[]> GenerateReportPdfAsync(string reportType, DateTime dateFrom, DateTime dateTo, object reportData);
+    }
+}
