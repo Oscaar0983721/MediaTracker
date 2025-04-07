714e7366bad0dc98db0150ae72471ba8cf16a3c0
Andrés Araya Saborío <142752659+Oscaar0983721@users.noreply.github.com>
2025-04-06 18:52:25 -0600

    Create IMediaRepository.cs

---
+++ MediaTracker.API/Repositories/Interfaces/IMediaRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MediaTracker.API.Models;
+
+namespace MediaTracker.API.Repositories.Interfaces
+{
+    public interface IMediaRepository
+    {
+        Task<List<Media>> GetAllMoviesAsync();
+        Task<List<Media>> GetAllSeriesAsync();
+        Task<Media> GetByIdAsync(int id);
+        Task<List<Media>> GetByGenreAsync(string genre);
+        Task<List<Media>> SearchAsync(string query);
+        Task<Media> AddAsync(Media media);
+        Task<Media> UpdateAsync(Media media);
+        Task<bool> DeleteAsync(int id);
+    }
+}
