bda0ddd7ac6a5ea32d373b89c3204902b74325f8
Andrés Araya Saborío <142752659+Oscaar0983721@users.noreply.github.com>
2025-04-06 18:54:41 -0600

    Create UserRepository.cs

---
+++ MediaTracker.API/Repositories/UserRepository.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediaTracker.API.Models;
+using MediaTracker.API.Repositories.Interfaces;
+using MediaTracker.API.Utils;
+
+namespace MediaTracker.API.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly JsonFileManager<User> _fileManager;
+
+        public UserRepository()
+        {
+            _fileManager = new JsonFileManager<User>("Data/users.json");
+        }
+
+        public async Task<List<User>> GetAllAsync()
+        {
+            return await _fileManager.ReadAllAsync();
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            var users = await GetAllAsync();
+            return users.FirstOrDefault(u => u.Id == id);
+        }
+
+        public async Task<User> GetByUsernameAsync(string username)
+        {
+            var users = await GetAllAsync();
+            return users.FirstOrDefault(u => u.Username == username);
+        }
+
+        public async Task<User> GetByEmailAsync(string email)
+        {
+            var users = await GetAllAsync();
+            return users.FirstOrDefault(u => u.Email == email);
+        }
+
+        public async Task<User> AddAsync(User user)
+        {
+            var users = await GetAllAsync();
+            user.Id = users.Count > 0 ? users.Max(u => u.Id) + 1 : 1;
+            user.CreatedAt = DateTime.Now;
+
+            users.Add(user);
+            await _fileManager.WriteAllAsync(users);
+
+            return user;
+        }
+
+        public async Task<User> UpdateAsync(User user)
+        {
+            var users = await GetAllAsync();
+            var index = users.FindIndex(u => u.Id == user.Id);
+
+            if (index >= 0)
+            {
+                users[index] = user;
+                await _fileManager.WriteAllAsync(users);
+                return user;
+            }
+
+            return null;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var users = await GetAllAsync();
+            var index = users.FindIndex(u => u.Id == id);
+
+            if (index >= 0)
+            {
+                users.RemoveAt(index);
+                await _fileManager.WriteAllAsync(users);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
