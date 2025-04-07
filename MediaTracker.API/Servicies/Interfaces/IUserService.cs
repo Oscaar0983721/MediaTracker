5f504c65e2cb47c77f3956162818e4763960a806
Andrés Araya Saborío <206283716+Roxana0976@users.noreply.github.com>
2025-04-06 18:44:33 -0600

    Create IUserService.cs

---
+++ MediaTracker.API/Servicies/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MediaTracker.API.Models;
+
+namespace MediaTracker.API.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<User> GetByIdAsync(int id);
+        Task<User> GetByUsernameAsync(string username);
+        Task<List<Media>> GetWatchlistAsync(int userId);
+        Task<WatchlistItem> AddToWatchlistAsync(int userId, int mediaId, string mediaType);
+        Task<bool> RemoveFromWatchlistAsync(int userId, int mediaId);
+    }
+}
