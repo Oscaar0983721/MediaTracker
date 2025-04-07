d4dbcd8e8133973d1a1d89ac8936febf749671d6
Andrés Araya Saborío <142752659+Oscaar0983721@users.noreply.github.com>
2025-04-06 18:52:22 -0600

    Create ICommentRepository.cs

---
+++ MediaTracker.API/Repositories/Interfaces/ICommentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MediaTracker.API.Models;
+
+namespace MediaTracker.API.Repositories.Interfaces
+{
+    public interface ICommentRepository
+    {
+        Task<List<Comment>> GetAllAsync();
+        Task<Comment> GetByIdAsync(int id);
+        Task<List<Comment>> GetByMediaIdAsync(int mediaId);
+        Task<List<Comment>> GetByUserIdAsync(int userId);
+        Task<Comment> AddAsync(Comment comment);
+        Task<Comment> UpdateAsync(Comment comment);
+        Task<bool> DeleteAsync(int id);
+    }
+}
