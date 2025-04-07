2eb7796b4e641e58df02bdef6b031434c3db98dd
Andrés Araya Saborío <142752659+Oscaar0983721@users.noreply.github.com>
2025-04-06 18:54:46 -0600

    Create PdfGenerator.cs

---
+++ MediaTracker.API/Utils/PdfGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using MediaTracker.API.Models;
+
+namespace MediaTracker.API.Utils
+{
+    public class PdfGenerator
+    {
+        public async Task<byte[]> GenerateReportPdfAsync(string reportType, DateTime dateFrom, DateTime dateTo, object reportData)
+        {
+            // In a real application, we would use a PDF library like iTextSharp or PDFsharp
+            // For this example, we'll just create a simple text representation
+
+            string reportText = $@"
+Report Type: {reportType}
+Date Range: {dateFrom.ToShortDateString()} - {dateTo.ToShortDateString()}
+Generated: {DateTime.Now.ToShortDateString()}
+
+Summary:
+{string.Join(Environment.NewLine, (reportData as dynamic).Summary)}
+
+Data:
+";
+
+            // Convert to bytes (in a real app, this would be PDF binary data)
+            return System.Text.Encoding.UTF8.GetBytes(reportText);
+        }
+    }
+}
