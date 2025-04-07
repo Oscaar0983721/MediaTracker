6935b9a15f87fd5624e13c5bb493c97fd2947b52
Andrés Araya Saborío <142752659+Oscaar0983721@users.noreply.github.com>
2025-04-06 18:52:27 -0600

    Create IUserRepository.cs

---
+++ MediaTracker.API/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MediaTracker.API.Models;
+
+namespace MediaTracker.API.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetAllAsync();
+        Task<User> GetByIdAsync(int id);
+        Task<User> GetByUsernameAsync(string username);
+        Task<User> GetByEmailAsync(string email);
+        Task<User> AddAsync(User user);
+        Task<User> UpdateAsync(User user);
+        Task<bool> DeleteAsync(int id);
+    }
+}
