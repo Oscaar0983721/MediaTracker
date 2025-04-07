f6c5834a9df66e9e2e7f476489a40af03a2f221c
Andrés Araya Saborío <206283716+Roxana0976@users.noreply.github.com>
2025-04-06 18:44:29 -0600

    Create IMediaService.cs

---
+++ MediaTracker.API/Servicies/Interfaces/IMediaService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MediaTracker.API.Models;
+
+namespace MediaTracker.API.Services.Interfaces
+{
+    public interface IMediaService
+    {
+        Task<List<Media>> GetAllMoviesAsync();
+        Task<List<Media>> GetAllSeriesAsync();
+        Task<Media> GetByIdAsync(int id);
+        Task<List<Media>> GetByGenreAsync(string genre);
+        Task<List<Media>> SearchAsync(string query);
+        Task<Media> AddAsync(Media media);
+        Task<Media> UpdateAsync(Media media);
+        Task<bool> DeleteAsync(int id);
+        Task<List<Comment>> GetCommentsAsync(int mediaId);
+        Task<Comment> AddCommentAsync(int mediaId, int userId, string content);
+    }
+}
