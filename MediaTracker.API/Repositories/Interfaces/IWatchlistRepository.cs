637060cd1e6e2cd9937280a4dd5e020954a59887
Andrés Araya Saborío <142752659+Oscaar0983721@users.noreply.github.com>
2025-04-06 18:52:29 -0600

    Create IWatchlistRepository.cs

---
+++ MediaTracker.API/Repositories/Interfaces/IWatchlistRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MediaTracker.API.Models;
+
+namespace MediaTracker.API.Repositories.Interfaces
+{
+    public interface IWatchlistRepository
+    {
+        Task<List<WatchlistItem>> GetAllAsync();
+        Task<List<WatchlistItem>> GetByUserIdAsync(int userId);
+        Task<WatchlistItem> GetByUserAndMediaIdAsync(int userId, int mediaId);
+        Task<WatchlistItem> AddAsync(WatchlistItem watchlistItem);
+        Task<bool> DeleteAsync(int userId, int mediaId);
+    }
+}
